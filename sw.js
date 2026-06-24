01435de35cf974cf0fc7a067699aaa65fc7d02e2
dyt41410-stack <dyt41410@gmail.com>
2026-06-24 17:57:01 -0500

    Add service worker

---
+++ sw.js
@@ -0,0 +1,32 @@
+const CACHE = 'nemu-v1';
+const ASSETS = [
+  '/telefono-nemu/',
+  '/telefono-nemu/index.html',
+  '/telefono-nemu/manifest.json',
+  '/telefono-nemu/icon-192.png',
+  '/telefono-nemu/icon-512.png'
+];
+
+self.addEventListener('install', function(e) {
+  e.waitUntil(
+    caches.open(CACHE).then(function(c) { return c.addAll(ASSETS); })
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', function(e) {
+  e.waitUntil(
+    caches.keys().then(function(keys) {
+      return Promise.all(keys.filter(function(k){ return k !== CACHE; }).map(function(k){ return caches.delete(k); }));
+    })
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', function(e) {
+  e.respondWith(
+    caches.match(e.request).then(function(r) {
+      return r || fetch(e.request);
+    })
+  );
+});
